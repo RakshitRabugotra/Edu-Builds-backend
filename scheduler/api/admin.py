0163612b8f0097fa4b2b1f5efde6deb20455b6e8
Rakshit Rabugotra <90450441+RakshitRabugotra@users.noreply.github.com>
2023-01-07 21:34:19 +0530

    Add dummy user auth

--- scheduler/api/admin.py
+++ scheduler/api/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import MyUser, Course
 
 # Register your models here.
+@admin.register(MyUser)
+class MyUserAdmin(admin.ModelAdmin):
+    list_display = ['id', 'username', 'user_email', 'created_on']
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'url', 'duration', 'user_id']
--- scheduler/api/views.py
+++ scheduler/api/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework import generics
 from .serializers import CourseSerializer, MyUserSerializer
 from .models import Course, MyUser
+from django.contrib.auth import authenticate, login
 
 class CourseView(generics.CreateAPIView):
     queryset = Course.objects.all()
@@ -11,3 +12,17 @@ class CourseView(generics.CreateAPIView):
 class MyUserView(generics.CreateAPIView):
     queryset = MyUser.objects.all()
     serializer_class = MyUserSerializer
+
+
+# To authenticate the user on login
+def user_auth(request):
+    username = request.POST['username']
+    password = request.POST['password']
+
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        # Redirect to a successful page
+        ...
+    else:
+        # Return an 'invalid login' error message
+        ...
